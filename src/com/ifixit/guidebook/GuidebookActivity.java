3aaf40d63bb67a054c972be683af2d30d09ceddb
Marc Zych <marc@ifixit.com>
2011-09-07 19:56:40 -0700

    The main activity now launches a WebView to view www.ifixit.com and intercepts guide URLs and displays them natively on the device
    git-svn-id: file:///var/ifixit/CodeRepos/Apps/Android/Guidebook@13399 3574e4b1-9a2c-0410-9c1c-83071a6f58f1

--- src/com/ifixit/guidebook/GuidebookActivity.java
+++ src/com/ifixit/guidebook/GuidebookActivity.java
@@ -4,17 +4,26 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.webkit.WebView;
+
 public class GuidebookActivity extends Activity {
+   protected WebView mWebView;
+
    @Override
    public void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
-      
+
+      setContentView(R.layout.webview);
+      mWebView = (WebView)findViewById(R.id.webView);
+      mWebView.getSettings().setJavaScriptEnabled(true);
+      mWebView.loadUrl("http://www.ifixit.com");
+      mWebView.setWebViewClient(new GuideWebView(this));
+   }
+
+   public void viewGuide(int guideid) {
       Intent intent = new Intent(this, GuideView.class);
 
-      //intent.putExtra("guideid", 4531);
-
-       intent.putExtra("guideid", 3550);
-      // xbox RROD
+      intent.putExtra("guideid", guideid);
       startActivity(intent);
    }
 }
