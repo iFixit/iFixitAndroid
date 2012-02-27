40be3f5045090505d369ecb2ad73a2b59615de36
Marc Zych <marczych@gmail.com>
2012-02-26 22:03:52 -0800

    Show device list and device view fragments side by side only on tablets

--- App/src/com/ifixit/android/ifixit/TopicViewActivity.java
+++ App/src/com/ifixit/android/ifixit/TopicViewActivity.java
@@ -13,16 +13,20 @@ public class TopicViewActivity extends FragmentActivity {
    public void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
 
-      if (getResources().getConfiguration().orientation ==
+      setContentView(R.layout.topic_view);
+
+      mTopicView = (TopicViewFragment)getSupportFragmentManager()
+       .findFragmentById(R.id.topic_view_fragment);
+
+      // We can handle the fragments side by side in the previous activity
+      // so lets go back there
+      if ((mTopicView == null || !mTopicView.isInLayout()) &&
+       getResources().getConfiguration().orientation ==
        Configuration.ORIENTATION_LANDSCAPE) {
          finish();
          return;
       }
 
-      setContentView(R.layout.topic_view);
-
-      mTopicView = (TopicViewFragment)getSupportFragmentManager()
-       .findFragmentById(R.id.topic_view_fragment);
       mTopicView.setTopicNode((TopicNode)getIntent().
        getSerializableExtra(TOPIC_KEY));
    }
