0ab989b773d812cc3579926a657104c88506af53
Marc Zych <marczych@gmail.com>
2012-03-03 18:13:57 -0800

    Remove debug statements

--- App/src/com/ifixit/android/ifixit/MainApplication.java
+++ App/src/com/ifixit/android/ifixit/MainApplication.java
@@ -3,7 +3,6 @@ package com.ifixit.android.ifixit;
 import android.app.Application;
 import android.content.Context;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.WindowManager;
 
 public class MainApplication extends Application {
@@ -26,23 +25,10 @@ public class MainApplication extends Application {
          wm.getDefaultDisplay().getMetrics(metrics);
          int maxDimension = Math.max(metrics.heightPixels, metrics.widthPixels);
 
-         // Left in for testing purposes
-         Log.w("iFixit", "density: " + metrics.density);
-         Log.w("iFixit", "densityDpi: " + metrics.densityDpi);
-         Log.w("iFixit", "heightPixels: " + metrics.heightPixels);
-         Log.w("iFixit", "scaledDensity: " + metrics.scaledDensity);
-         Log.w("iFixit", "widthPixels: " + metrics.widthPixels);
-         Log.w("iFixit", "xdpi: " + metrics.xdpi);
-         Log.w("iFixit", "ydpi: " + metrics.ydpi);
-
-         Log.w("iFixit", "dim/res: " + (maxDimension / metrics.density));
-
          // Larger screen = larger images
          if ((maxDimension / metrics.density) > 800) {
-            Log.w("iFixit", "Large images");
             mImageSizes = new ImageSizes(".large", ".large", ".huge");
          } else {
-            Log.w("iFixit", "Small images");
             mImageSizes = new ImageSizes(".medium", ".medium", ".large");
          }
       }
