de50838208d5448ec37e600d3c43cf292ed6a74e
Marc Zych <marc@ifixit.com>
2012-03-15 11:52:04 -0700

    Add ProgressBar to LoadingFragment

--- App/src/com/dozuki/ifixit/LoadingFragment.java
+++ App/src/com/dozuki/ifixit/LoadingFragment.java
@@ -1,22 +1,14 @@
 package com.dozuki.ifixit;
 
 import android.os.Bundle;
-
 import android.support.v4.app.Fragment;
-
 import android.view.LayoutInflater;
-import android.view.ViewGroup;
 import android.view.View;
-
-import android.widget.TextView;
+import android.view.ViewGroup;
 
 public class LoadingFragment extends Fragment {
    public View onCreateView(LayoutInflater inflater, ViewGroup container,
     Bundle savedInstanceState) {
-      TextView textView = new TextView(getActivity());
-
-      textView.setText("Loading...");
-
-      return textView;
+      return inflater.inflate(R.layout.loading_fragment, container, false);
    }
 }
