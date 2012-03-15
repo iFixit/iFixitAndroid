70fa53b032c6e503973c4c79a932e5ddd0fba4df
Timothy Asp <timothy.asp@gmail.com>
2012-03-15 11:33:43 -0700

    List next icon only shows on categories.  Also some header style tweaks

--- App/src/com/dozuki/ifixit/TopicListRow.java
+++ App/src/com/dozuki/ifixit/TopicListRow.java
@@ -2,13 +2,15 @@ package com.dozuki.ifixit;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class TopicListRow extends LinearLayout {
    private TextView mTopicName;
    private TopicNode mTopic;
-
+   private ImageView mNextIcon;
+   
    public TopicListRow(Context context) {
       super(context);
 
@@ -17,10 +19,17 @@ public class TopicListRow extends LinearLayout {
       inflater.inflate(R.layout.topic_list_row, this, true);
 
       mTopicName = (TextView)findViewById(R.id.topic_title);
+      mNextIcon = (ImageView)findViewById(R.id.next_icon);
    }
 
    public void setTopic(TopicNode topic) {
       mTopic = topic;
       mTopicName.setText(mTopic.getName());
+      
+      if (!mTopic.isLeaf()) {
+         mNextIcon.setVisibility(VISIBLE);
+      } else {
+    	 mNextIcon.setVisibility(GONE);
+      }
    }
 }
