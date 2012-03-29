7721f4fcf7d979383e366e2aab6d3afabf8a6849
Marc Zych <marczych@gmail.com>
2012-03-28 22:13:09 -0700

    Remove ImageZoomLibrary

--- ImageZoomLibrary/src/it/sephiroth/android/library/imagezoom/IDisposable.java
+++
@@ -1,5 +0,0 @@
-package it.sephiroth.android.library.imagezoom;
-
-public interface IDisposable {
-	void dispose();
-}
--- ImageZoomLibrary/src/it/sephiroth/android/library/imagezoom/easing/Cubic.java
+++
@@ -1,9 +0,0 @@
-package it.sephiroth.android.library.imagezoom.easing;
-
-public class Cubic {
-	
-	public static float easeOut( float time, float start, float end, float duration )
-	{
-		return end * ( ( time = time / duration - 1 ) * time * time + 1 ) + start;
-	}
-}
