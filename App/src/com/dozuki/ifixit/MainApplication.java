79a91960141db73168a0b7aa1d344d85bfccc6b0
Marc Zych <marc@ifixit.com>
2012-04-01 19:56:37 -0700

    Fix full screen image view

--- App/src/com/dozuki/ifixit/MainApplication.java
+++ App/src/com/dozuki/ifixit/MainApplication.java
@@ -2,10 +2,14 @@ package com.dozuki.ifixit;
 
 import android.app.Application;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.util.DisplayMetrics;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 import com.ifixit.android.imagemanager.ImageManager;
+
+import it.sephiroth.android.library.imagezoom.ImageViewTouch;
 
 public class MainApplication extends Application {
    public static final int SIZE_CUTOFF = 800;
@@ -16,6 +20,28 @@ public class MainApplication extends Application {
    public ImageManager getImageManager() {
       if (mImageManager == null) {
          mImageManager = new ImageManager(this);
+
+         mImageManager.setController(new ImageManager.Controller() {
+            public boolean overrideDisplay(String url, ImageView imageView) {
+               return false;
+            }
+
+            public void loading(ImageView imageView) {
+            }
+
+            public boolean displayImage(ImageView imageView, Bitmap bitmap,
+             String url) {
+               if (imageView instanceof ImageViewTouch) {
+                  ((ImageViewTouch)imageView).setImageBitmapReset(bitmap, true);
+                  return true;
+               }
+
+               return false;
+            }
+
+            public void fail(ImageView imageView) {
+            }
+         });
       }
 
       return mImageManager;
