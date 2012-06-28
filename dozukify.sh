5a35d4b65d49c1be2597e2a2f433a86f93eb9ad1
Marc Zych <marc@ifixit.com>
2012-06-27 17:56:12 -0700

    Update dozukify.sh to change resources

--- dozukify.sh
+++ dozukify.sh
@@ -2,7 +2,7 @@
 
 if [ $# -eq 0 ]
 then
-   echo "Must specify a site name!"
+   echo "Usage: $0 <site name> [--package-change]"
    exit;
 fi
 
@@ -10,7 +10,28 @@ siteName=$1
 
 cd App/
 
-sed -i "" -e "s/com.dozuki.ifixit/com.dozuki.$siteName/" `find . -name "*.java"` `find . -name "*.xml"`
-mv src/com/dozuki/ifixit src/com/dozuki/$siteName
+if [ "$2" = "--package-change" ]
+then
+   # Update the package name for all relevant files
+   sed -i "" -e "s/com.dozuki.ifixit/com.dozuki.$siteName/" `find ./src -name "*.java"` `find . -name "*.xml"`
+   mv src/com/dozuki/ifixit src/com/dozuki/$siteName
+fi
+
+# Update logo and app name
+sed -i "" -e "s/_ifixit/_$siteName/" ./AndroidManifest.xml
+
+# Update intent-filter for GuideView
+sed -i "" -e "s/www\.ifixit\.com/$siteName.dozuki.com/" ./AndroidManifest.xml
+
+# Change the default activity to the site list for Dozuki
+if [ "$siteName" = "dozuki" ]
+then
+   sed -i "" -e "s/TopicsActivity/TopicsActivityTmp/" ./AndroidManifest.xml
+   sed -i "" -e "s/SiteListActivity/TopicsActivity/" ./AndroidManifest.xml
+   sed -i "" -e "s/TopicsActivityTmp/SiteListActivity/" ./AndroidManifest.xml
+
+   # Update the theme
+   sed -i "" -e "s/Theme\.iFixit/Theme.Dozuki/" ./AndroidManifest.xml
+fi
 
 cd -
