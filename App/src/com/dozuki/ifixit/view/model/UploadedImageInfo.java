cebf7aabce8c7c4a58cf22b5d0d1234e64c4b419
Marc Zych <marczych@gmail.com>
2012-10-19 18:15:07 -0700

    Code cleanupf or UploadedImageInfo.

--- App/src/com/dozuki/ifixit/view/model/UploadedImageInfo.java
+++ App/src/com/dozuki/ifixit/view/model/UploadedImageInfo.java
@@ -5,31 +5,31 @@ import java.io.Serializable;
 public class UploadedImageInfo implements Serializable {
    private static final long serialVersionUID = -4960272337013311382L;
 
-   String mGuid;
-   String mImageid;
-   String status;
+   private String mGuid;
+   private String mImageid;
+   private String mStatus;
 
    public String getmGuid() {
       return mGuid;
    }
 
-   public void setmGuid(String mGuid) {
-      this.mGuid = mGuid;
+   public void setmGuid(String guid) {
+      mGuid = mGuid;
    }
 
    public String getmImageid() {
       return mImageid;
    }
 
-   public void setmImageid(String mImageid) {
-      this.mImageid = mImageid;
+   public void setmImageid(String imageid) {
+      mImageid = mImageid;
    }
 
    public String getStatus() {
-      return status;
+      return mStatus;
    }
 
    public void setStatus(String status) {
-      this.status = status;
+      mStatus = status;
    }
 }
