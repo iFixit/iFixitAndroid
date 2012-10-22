12abff3b8ee30921283026e91eea3a1e718d7eae
Marc Zych <marczych@gmail.com>
2012-10-21 19:05:55 -0700

    Minor code cleanup for UserImageList

--- App/src/com/dozuki/ifixit/gallery/model/UserImageList.java
+++ App/src/com/dozuki/ifixit/gallery/model/UserImageList.java
@@ -5,20 +5,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
-import com.dozuki.ifixit.login.model.User;
-
 public class UserImageList implements Serializable {
    private static final long serialVersionUID = 7067096480019401662L;
 
-   private User mUser;
    private ArrayList<UserImageInfo> mImages;
 
    public UserImageList() {
       mImages = new ArrayList<UserImageInfo>();
-   }
-
-   public User getUser() {
-      return mUser;
    }
 
    public void addImage(UserImageInfo userImageInfo) {
@@ -28,16 +21,12 @@ public class UserImageList implements Serializable {
       Collections.sort(mImages, new UserImageInfoComparator());
    }
 
-   public void setUser(User mUser) {
-      this.mUser = mUser;
-   }
-
    public ArrayList<UserImageInfo> getImages() {
       return mImages;
    }
 
-   public void setImages(ArrayList<UserImageInfo> mImages) {
-      this.mImages = mImages;
+   public void setImages(ArrayList<UserImageInfo> images) {
+      mImages = images;
    }
 
    private static class UserImageInfoComparator implements
@@ -53,7 +42,7 @@ public class UserImageList implements Serializable {
             return -1;
 
          return Integer.parseInt(e1.getImageid()) - Integer.parseInt(
-          e2 .getImageid());
+          e2.getImageid());
       }
    }
 }
