2929b20fffd459804e7fd00c09ceefefe578fe71
Aaron Rivera <rivera.aaron1@gmail.com>
2013-01-24 15:02:56 -0800

    code cleanup

--- App/src/com/dozuki/ifixit/guide_create/ui/GuideCreateStepsActivity.java
+++ App/src/com/dozuki/ifixit/guide_create/ui/GuideCreateStepsActivity.java
@@ -2,36 +2,30 @@ package com.dozuki.ifixit.guide_create.ui;
 
 import java.util.ArrayList;
 
-import org.holoeverywhere.app.Activity;
-
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
-import com.actionbarsherlock.view.SubMenu;
 import com.dozuki.ifixit.MainApplication;
 import com.dozuki.ifixit.R;
-import com.dozuki.ifixit.gallery.ui.GalleryActivity;
 import com.dozuki.ifixit.guide_create.model.GuideCreateObject;
 import com.dozuki.ifixit.guide_create.model.GuideCreateStepObject;
 import com.dozuki.ifixit.guide_create.ui.GuideIntroFragment.GuideCreateIntroListener;
-import com.dozuki.ifixit.topic_view.ui.TopicsActivity;
 import com.dozuki.ifixit.util.IfixitActivity;
 
 public class GuideCreateStepsActivity extends IfixitActivity implements GuideCreateIntroListener {
-	static final int GUIDE_EDIT_STEP_REQUEST = 0;
-	private static final String SHOWING_HELP = "SHOWING_HELP";
-	public static String GuideKey = "GuideKey";
-	private ActionBar mActionBar;
-	private GuideCreateStepPortalFragment mStepPortalFragment;
-	private ArrayList<GuideCreateStepObject> mStepList;
-	private GuideCreateObject mGuide;
+   static final int GUIDE_EDIT_STEP_REQUEST = 0;
+   private static final String SHOWING_HELP = "SHOWING_HELP";
+   public static String GuideKey = "GuideKey";
+   private ActionBar mActionBar;
+   private GuideCreateStepPortalFragment mStepPortalFragment;
+   private ArrayList<GuideCreateStepObject> mStepList;
+   private GuideCreateObject mGuide;
    private boolean mShowingHelp;
 
 	public ArrayList<GuideCreateStepObject> getStepList() {
@@ -50,146 +44,127 @@ public class GuideCreateStepsActivity extends IfixitActivity implements GuideCre
 		return mGuide;
 	}
 
-	@SuppressWarnings("unchecked")
-	public void onCreate(Bundle savedInstanceState) {
-		setTheme(((MainApplication) getApplication()).getSiteTheme());
-		getSupportActionBar().setTitle(
-				((MainApplication) getApplication()).getSite().mTitle);
-		mActionBar = getSupportActionBar();
-		mActionBar.setTitle("");
-		prepareNavigationSpinner(mActionBar);
-	   this.getSupportActionBar().setSelectedNavigationItem(1);
-		
-		Bundle extras = getIntent().getExtras();
-		if (extras != null) {
-			mGuide = (GuideCreateObject) extras
-					.getSerializable(GuideCreateStepsActivity.GuideKey);
-			if (mGuide.getSteps() == null)
-				mGuide.setStepList(new ArrayList<GuideCreateStepObject>());
-			mStepList = mGuide.getSteps();
-		} 
-		if (savedInstanceState != null) {
-			mStepList = mGuide.getSteps();
-			mShowingHelp = savedInstanceState.getBoolean(SHOWING_HELP);
+   @SuppressWarnings("unchecked")
+   public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setTheme(((MainApplication) getApplication()).getSiteTheme());
+      getSupportActionBar().setTitle(((MainApplication) getApplication()).getSite().mTitle);
+      mActionBar = getSupportActionBar();
+      mActionBar.setTitle("");
+      prepareNavigationSpinner(mActionBar);
+      this.getSupportActionBar().setSelectedNavigationItem(CREATE_GUIDES);
+
+      Bundle extras = getIntent().getExtras();
+      if (extras != null) {
+         mGuide = (GuideCreateObject) extras.getSerializable(GuideCreateStepsActivity.GuideKey);
+         if (mGuide.getSteps() == null)
+            mGuide.setStepList(new ArrayList<GuideCreateStepObject>());
+         mStepList = mGuide.getSteps();
+      }
+      if (savedInstanceState != null) {
+         mStepList = mGuide.getSteps();
+         mShowingHelp = savedInstanceState.getBoolean(SHOWING_HELP);
          if (mShowingHelp)
             createHelpDialog().show();
-		}
-
-		super.onCreate(savedInstanceState);
-
-		setContentView(R.layout.guide_create_steps_root);
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		String tag = "guide_steeps_portal_fragment";
-		if (findViewById(R.id.guide_create_fragment_steps_container) != null
-				&& getSupportFragmentManager().findFragmentByTag(tag) == null) {
-			mStepPortalFragment = new GuideCreateStepPortalFragment(mGuide);
-			mStepPortalFragment.setRetainInstance(true);
-			getSupportFragmentManager()
-					.beginTransaction()
-					.add(R.id.guide_create_fragment_steps_container,
-							mStepPortalFragment, tag).commit();
-		}
-	}
+      }
+      setContentView(R.layout.guide_create_steps_root);
+      getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+      String tag = "guide_steeps_portal_fragment";
+      if (findViewById(R.id.guide_create_fragment_steps_container) != null
+         && getSupportFragmentManager().findFragmentByTag(tag) == null) {
+         mStepPortalFragment = new GuideCreateStepPortalFragment(mGuide);
+         mStepPortalFragment.setRetainInstance(true);
+         getSupportFragmentManager().beginTransaction()
+            .add(R.id.guide_create_fragment_steps_container, mStepPortalFragment, tag).commit();
+      }
+   }
 
    @Override
    public boolean onCreateOptionsMenu(Menu menu) {
-
       MenuInflater inflater = getSupportMenuInflater();
       inflater.inflate(R.menu.step_create_menu, menu);
-
       return super.onCreateOptionsMenu(menu);
    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			finish();
-			return true;
-		case R.id.help_button:
-         createHelpDialog().show();
-         return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-	}
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item) {
+      switch (item.getItemId()) {
+         case android.R.id.home:
+            finish();
+            return true;
+         case R.id.help_button:
+            createHelpDialog().show();
+            return true;
+         default:
+            return super.onOptionsItemSelected(item);
+      }
+   }
 
-	@Override 
-	public void onSaveInstanceState(Bundle savedInstanceState) {	
-		savedInstanceState.putSerializable(GuideCreateStepsActivity.GuideKey,
-				mGuide);
-		savedInstanceState.putBoolean(SHOWING_HELP, mShowingHelp);
-		super.onSaveInstanceState(savedInstanceState);
-	}
-	
-	@Override
-	public void finish()
-	{
-		Intent returnIntent = new Intent();
-		returnIntent.putExtra(GuideCreateActivity.GUIDE_KEY, mGuide);
-		setResult(RESULT_OK, returnIntent);
-		super.finish();
-	}
+   @Override
+   public void onSaveInstanceState(Bundle savedInstanceState) {
+      savedInstanceState.putSerializable(GuideCreateStepsActivity.GuideKey, mGuide);
+      savedInstanceState.putBoolean(SHOWING_HELP, mShowingHelp);
+      super.onSaveInstanceState(savedInstanceState);
+   }
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		if (requestCode == GUIDE_EDIT_STEP_REQUEST) {
-			if (resultCode == RESULT_OK) {
-				GuideCreateObject guide = (GuideCreateObject) data
-						.getSerializableExtra(GuideCreateActivity.GUIDE_KEY);
-				if (guide != null) {
-					mGuide = guide;
-					mStepList = mGuide.getSteps();
-				}
-			}
-		}
+   @Override
+   public void finish() {
+      Intent returnIntent = new Intent();
+      returnIntent.putExtra(GuideCreateActivity.GUIDE_KEY, mGuide);
+      setResult(RESULT_OK, returnIntent);
+      super.finish();
+   }
+
+   @Override
+   protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+      super.onActivityResult(requestCode, resultCode, data);
+      if (requestCode == GUIDE_EDIT_STEP_REQUEST) {
+         if (resultCode == RESULT_OK) {
+            GuideCreateObject guide = (GuideCreateObject) data.getSerializableExtra(GuideCreateActivity.GUIDE_KEY);
+            if (guide != null) {
+               mGuide = guide;
+               mStepList = mGuide.getSteps();
+            }
+         }
+      }
 	}
 	
-	 private AlertDialog createHelpDialog() {
-	      mShowingHelp = true;
-	      AlertDialog.Builder builder = new AlertDialog.Builder(this);
-	      builder
-	            .setTitle(getString(R.string.media_help_title))
-	            .setMessage(getString(R.string.guide_create_steps_help))
-	            .setPositiveButton(getString(R.string.media_help_confirm),
-	               new DialogInterface.OnClickListener() {
+   private AlertDialog createHelpDialog() {
+      mShowingHelp = true;
+      AlertDialog.Builder builder = new AlertDialog.Builder(this);
+      builder.setTitle(getString(R.string.media_help_title)).setMessage(getString(R.string.guide_create_steps_help))
+         .setPositiveButton(getString(R.string.media_help_confirm), new DialogInterface.OnClickListener() {
 
-	            public void onClick(DialogInterface dialog, int id) {
-	                     mShowingHelp = false;
-	                     dialog.cancel();
-	                  }
-	               });
+            public void onClick(DialogInterface dialog, int id) {
+               mShowingHelp = false;
+               dialog.cancel();
+            }
+         });
 
-	      AlertDialog dialog = builder.create();
-	      dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-	         @Override
-	         public void onDismiss(DialogInterface dialog) {
-	            mShowingHelp = false;
-	         }
-	      });
+      AlertDialog dialog = builder.create();
+      dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+         @Override
+         public void onDismiss(DialogInterface dialog) {
+            mShowingHelp = false;
+         }
+      });
 
-	      return dialog;
-	   }
-	
-	  
+      return dialog;
+   }
+
    @Override
    public void onFinishIntroInput(String device, String title, String summary, String intro, String guideType,
       String thing) {
-      
       mGuide.setTitle(title);
       mGuide.setTopic(device);
       mGuide.setSummary(summary);
       mGuide.setIntroduction(intro);
-            
-      //TODO PATCH GUIDE
-     // APIService.call((Activity) getActivity(),
-      //   APIService.getCreateGuideAPICall(device, title, summary, intro, guideType, thing));
-
+      // TODO PATCH GUIDE
+      // APIService.call((Activity) getActivity(),
+      // APIService.getCreateGuideAPICall(device, title, summary, intro, guideType, thing));
       getSupportFragmentManager().popBackStack();
-      
    }
-   
+
    public void onResume() {
       super.onResume();
       this.getSupportActionBar().setSelectedNavigationItem(1);
--- App/src/com/dozuki/ifixit/guide_create/ui/LockableViewPager.java
+++ App/src/com/dozuki/ifixit/guide_create/ui/LockableViewPager.java
@@ -7,32 +7,30 @@ import android.view.MotionEvent;
 
 public class LockableViewPager extends ViewPager {
 
-    private boolean isPagingEnabled;
+   private boolean isPagingEnabled;
 
-    public LockableViewPager(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        this.isPagingEnabled = true;
-    }
+   public LockableViewPager(Context context, AttributeSet attrs) {
+      super(context, attrs);
+      this.isPagingEnabled = true;
+   }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if (this.isPagingEnabled) {
-            return super.onTouchEvent(event);
-        }
+   @Override
+   public boolean onTouchEvent(MotionEvent event) {
+      if (this.isPagingEnabled) {
+         return super.onTouchEvent(event);
+      }
+      return false;
+   }
 
-        return false;
-    }
+   @Override
+   public boolean onInterceptTouchEvent(MotionEvent event) {
+      if (this.isPagingEnabled) {
+         return super.onInterceptTouchEvent(event);
+      }
+      return false;
+   }
 
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent event) {
-        if (this.isPagingEnabled) {
-            return super.onInterceptTouchEvent(event);
-        }
-
-        return false;
-    }
-
-    public void setPagingEnabled(boolean b) {
-        this.isPagingEnabled = b;
-    }
+   public void setPagingEnabled(boolean b) {
+      this.isPagingEnabled = b;
+   }
 }
