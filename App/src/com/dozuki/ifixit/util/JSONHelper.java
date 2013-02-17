f295f776cfababd8c171461639230d8b04ecbfe9
Marc Zych <marczych@gmail.com>
2013-02-17 00:51:33 -0800

    Fix user image json parsing

--- App/src/com/dozuki/ifixit/util/JSONHelper.java
+++ App/src/com/dozuki/ifixit/util/JSONHelper.java
@@ -287,9 +287,9 @@ public class JSONHelper {
     throws JSONException {
       UserImageInfo userImageInfo = new UserImageInfo();
       userImageInfo.setItemId(jImage.getString("imageid"));
-      userImageInfo.setGuid(jImage.getString("guid"));
-      userImageInfo.setHeight(jImage.getString("height"));
+      userImageInfo.setGuid(jImage.getString("base_url"));
       userImageInfo.setWidth(jImage.getString("width"));
+      userImageInfo.setHeight(jImage.getString("height"));
       userImageInfo.setRatio(jImage.getString("ratio"));
 
       return userImageInfo;
