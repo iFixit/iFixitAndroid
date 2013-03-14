f91d39331d90da9043d09f897f2fa5219796c2a9
Marc Zych <marczych@gmail.com>
2013-03-14 14:35:01 -0700

    Fix UserImageInfo parsing for Gallery
    Temporary fix but it works for now. All of the image parsing code
    will have to be refactored soon.

--- App/src/com/dozuki/ifixit/util/JSONHelper.java
+++ App/src/com/dozuki/ifixit/util/JSONHelper.java
@@ -287,8 +287,10 @@ public class JSONHelper {
    public static UserImageInfo parseUserImageInfo(JSONObject jImage)
     throws JSONException {
       UserImageInfo userImageInfo = new UserImageInfo();
-      userImageInfo.setItemId(jImage.getString("imageid"));
-      userImageInfo.setGuid(jImage.getString("base_url"));
+      // TODO: Make a function to parse the image format and return an object
+      // that UserImageInfo uses. All other image parsing should use that too.
+      userImageInfo.setItemId(jImage.getJSONObject("image").getString("id"));
+      userImageInfo.setGuid(jImage.getJSONObject("image").getString("original"));
       userImageInfo.setWidth(jImage.getString("width"));
       userImageInfo.setHeight(jImage.getString("height"));
       userImageInfo.setRatio(jImage.getString("ratio"));
