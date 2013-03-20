20aebd5684666496f69b9c4bd7df964140aa0745
Aaron Rivera <rivera.aaron1@gmail.com>
2013-03-20 10:37:46 -0700

    removed old todos

--- App/src/com/dozuki/ifixit/util/IfixitActivity.java
+++ App/src/com/dozuki/ifixit/util/IfixitActivity.java
@@ -342,19 +342,16 @@ public abstract class IfixitActivity extends Activity {
 
       @Override
       public int getCount() {
-         // TODO Auto-generated method stub
          return data.size();
       }
 
       @Override
       public Object getItem(int position) {
-         // TODO Auto-generated method stub
          return null;
       }
 
       @Override
       public long getItemId(int position) {
-         // TODO Auto-generated method stub
          return 0;
       }
    }
