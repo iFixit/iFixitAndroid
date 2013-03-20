392e454fe6640050fd65766d26fcf8a14b42e8ee
Aaron Rivera <rivera.aaron1@gmail.com>
2013-03-19 23:20:01 -0700

    fixed the issue with loading and holding the device upside down

--- App/src/com/dozuki/ifixit/util/IfixitActivity.java
+++ App/src/com/dozuki/ifixit/util/IfixitActivity.java
@@ -5,10 +5,13 @@ import java.util.ArrayList;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app._HoloActivity;
+import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
@@ -371,5 +374,61 @@ public abstract class IfixitActivity extends Activity {
          this.title = title;
          this.id = id;
       }
+   }
+
+   /**  this determines the orientation of the current screen properly
+    *  http://stackoverflow.com/a/10383164 **/
+
+   protected int getScreenOrientation() {
+      int rotation = getWindowManager().getDefaultDisplay().getRotation();
+      DisplayMetrics dm = new DisplayMetrics();
+      getWindowManager().getDefaultDisplay().getMetrics(dm);
+      int width = dm.widthPixels;
+      int height = dm.heightPixels;
+      int orientation;
+      // if the device's natural orientation is portrait:
+      if ((rotation == Surface.ROTATION_0 || rotation == Surface.ROTATION_180) && height > width
+         || (rotation == Surface.ROTATION_90 || rotation == Surface.ROTATION_270) && width > height) {
+         switch (rotation) {
+            case Surface.ROTATION_0:
+               orientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+               break;
+            case Surface.ROTATION_90:
+               orientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+               break;
+            case Surface.ROTATION_180:
+               orientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
+               break;
+            case Surface.ROTATION_270:
+               orientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
+               break;
+            default:
+               orientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+               break;
+         }
+      }
+      // if the device's natural orientation is landscape or if the device
+      // is square:
+      else {
+         switch (rotation) {
+            case Surface.ROTATION_0:
+               orientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+               break;
+            case Surface.ROTATION_90:
+               orientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+               break;
+            case Surface.ROTATION_180:
+               orientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
+               break;
+            case Surface.ROTATION_270:
+               orientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
+               break;
+            default:
+               orientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+               break;
+         }
+      }
+
+      return orientation;
    }
 }
