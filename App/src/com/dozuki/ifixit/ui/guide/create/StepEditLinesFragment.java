d3dc5a368eb51e81b44fa6f5780e925a3f2b81ec
Tim Asp <timothy.asp@gmail.com>
2013-06-07 11:30:30 -0700

    Add missing imports

--- App/src/com/dozuki/ifixit/ui/guide/create/StepEditLinesFragment.java
+++ App/src/com/dozuki/ifixit/ui/guide/create/StepEditLinesFragment.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
