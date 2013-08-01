24719ce981fe6a84c4d406cfb032d54aa0d35e7e
Marc Zych <marczych@gmail.com>
2013-08-01 16:23:00 -0700

    Guide view: Fix crash if parts but not tools exist

--- App/src/com/dozuki/ifixit/ui/guide/view/GuideViewAdapter.java
+++ App/src/com/dozuki/ifixit/ui/guide/view/GuideViewAdapter.java
@@ -4,23 +4,36 @@ package com.dozuki.ifixit.ui.guide.view;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+
 import com.dozuki.ifixit.MainApplication;
 import com.dozuki.ifixit.R;
 import com.dozuki.ifixit.model.guide.Guide;
 
 public class GuideViewAdapter extends FragmentStatePagerAdapter {
    private static final int GUIDE_INTRO_POSITION = 0;
-   private static final int GUIDE_TOOL_POSITION = 1;
-   private static final int GUIDE_PARTS_POSITION = 2;
+
    private int mStepOffset = 1;
+
+   // Default these to a page number that doesn't exist. They will be updated
+   // if the guide has tools and parts.
+   private int mToolsPosition = -1;
+   private int mPartsPosition = -1;
 
    private Guide mGuide;
 
    public GuideViewAdapter(FragmentManager fm, Guide guide) {
       super(fm);
       mGuide = guide;
-      if (guideHasParts()) mStepOffset++;
-      if (guideHasTools()) mStepOffset++;
+
+      if (guideHasTools()) {
+         mToolsPosition = mStepOffset;
+         mStepOffset++;
+      }
+
+      if (guideHasParts()) {
+         mPartsPosition = mStepOffset;
+         mStepOffset++;
+      }
    }
 
    @Override
@@ -34,39 +47,28 @@ public class GuideViewAdapter extends FragmentStatePagerAdapter {
 
    @Override
    public Fragment getItem(int position) {
-      switch (position) {
-         case GUIDE_INTRO_POSITION:
-            return new GuideIntroViewFragment(mGuide);
-         case GUIDE_TOOL_POSITION:
-            if (!guideHasTools()) break;
-
-            return new GuidePartsToolsViewFragment(mGuide.getTools());
-         case GUIDE_PARTS_POSITION:
-            if (!guideHasParts()) break;
-
-            return new GuidePartsToolsViewFragment(mGuide.getParts());
+      if (position == GUIDE_INTRO_POSITION) {
+         return new GuideIntroViewFragment(mGuide);
+      } else if (position == mToolsPosition) {
+         return new GuidePartsToolsViewFragment(mGuide.getTools());
+      } else if (position == mPartsPosition) {
+         return new GuidePartsToolsViewFragment(mGuide.getParts());
+      } else {
+         return new GuideStepViewFragment(mGuide.getStep(position - mStepOffset));
       }
-      return new GuideStepViewFragment(mGuide.getStep(position - mStepOffset));
    }
 
    @Override
    public CharSequence getPageTitle(int position) {
-      switch (position) {
-         case GUIDE_INTRO_POSITION:
-            return MainApplication.get().getString(R.string.introduction);
-         case GUIDE_TOOL_POSITION:
-            if (!guideHasTools()) break;
-
-            return MainApplication.get().getString(R.string.requiredTools);
-         case GUIDE_PARTS_POSITION:
-            if (!guideHasParts()) break;
-
-            return MainApplication.get().getString(R.string.requiredParts);
+      if (position == GUIDE_INTRO_POSITION) {
+         return MainApplication.get().getString(R.string.introduction);
+      } else if (position == mToolsPosition) {
+         return MainApplication.get().getString(R.string.requiredTools);
+      } else if (position == mPartsPosition) {
+         return MainApplication.get().getString(R.string.requiredParts);
+      } else {
+         return MainApplication.get().getString(R.string.step_number, (position - mStepOffset) + 1);
       }
-
-      // The step number must account for if there are parts or tools pages and the introduction page,
-      // and also should be 1 indexed.
-      return MainApplication.get().getString(R.string.step_number, (position - mStepOffset) + 1);
    }
 
    public int getStepOffset() {
