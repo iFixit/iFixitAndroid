b449581de4e912366f4b870c4f7588bce6b656b6
Marc Zych <marczych@gmail.com>
2013-08-11 21:28:59 -0700

    Move current site into gradle

--- App/src/com/dozuki/ifixit/MainApplication.java
+++ App/src/com/dozuki/ifixit/MainApplication.java
@@ -26,8 +26,6 @@ import java.net.URL;
 
 public class MainApplication extends Application {
    private static final int LARGE_SIZE_CUTOFF = 800;
-   // The current version of the app (this is replaced by dozukify.sh).
-   private static final String CURRENT_SITE = "SITE_ifixit";
 
    private static final String PREFERENCE_FILE = "PREFERENCE_FILE";
    private static final String FIRST_TIME_GALLERY_USER =
@@ -137,7 +135,7 @@ public class MainApplication extends Application {
     * title displayed.
     */
    public String getSiteDisplayTitle() {
-      if (CURRENT_SITE.equals("SITE_dozuki")) {
+      if (BuildConfig.SITE_NAME.equals("SITE_dozuki")) {
          return getSite().mTitle;
       } else {
          return "";
@@ -261,9 +259,7 @@ public class MainApplication extends Application {
     * (iFixit/Crucial etc.).
     */
    private Site getDefaultSite() {
-      String siteName = CURRENT_SITE.replace("SITE_", "");
-
-      return Site.getSite(siteName);
+      return Site.getSite(BuildConfig.SITE_NAME);
    }
 
    /**
