f6f0dd6647e2879d67c3aab033a33be32c32b69a
Tim Asp <timothy.asp@gmail.com>
2013-09-04 11:48:24 -0700

    Add missing log import

--- App/src/com/dozuki/ifixit/ui/BaseMenuDrawerActivity.java
+++ App/src/com/dozuki/ifixit/ui/BaseMenuDrawerActivity.java
@@ -7,6 +7,7 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
