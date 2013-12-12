e26826322d0b7c5d2bb19e8254515e59929b9f73
Tim Asp <timothy.asp@gmail.com>
2013-12-12 11:42:19 -0800

    Dimensions cleanup

--- App/src/com/dozuki/ifixit/ui/BaseActivity.java
+++ App/src/com/dozuki/ifixit/ui/BaseActivity.java
@@ -4,6 +4,7 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v4.app.Fragment;
