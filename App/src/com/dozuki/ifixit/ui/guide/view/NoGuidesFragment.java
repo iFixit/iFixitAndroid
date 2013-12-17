8567b7a9c836d8d78e0fe0480460d313248a4fad
Tim Asp <timothy.asp@gmail.com>
2013-12-17 11:30:40 -0800

    Add create new guide button on no guides fragment

--- App/src/com/dozuki/ifixit/ui/guide/view/NoGuidesFragment.java
+++ App/src/com/dozuki/ifixit/ui/guide/view/NoGuidesFragment.java
@@ -1,17 +1,29 @@
 package com.dozuki.ifixit.ui.guide.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.dozuki.ifixit.R;
 import com.dozuki.ifixit.ui.BaseFragment;
+import com.dozuki.ifixit.ui.guide.create.StepEditActivity;
 
 public class NoGuidesFragment extends BaseFragment {
    @Override
    public View onCreateView(LayoutInflater inflater, ViewGroup container,
     Bundle savedInstanceState) {
-      return inflater.inflate(R.layout.no_guides_fragment, container, false);
+      View v = inflater.inflate(R.layout.no_guides_fragment, container, false);
+
+      v.findViewById(R.id.new_guide_button).setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            Intent intent = new Intent(getActivity(), StepEditActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+         }
+      });
+
+      return v;
    }
 }
