5bbe8ad2dcbb8f52f14ad93395dcc90bd9c209f4
Marc Zych <marczych@gmail.com>
2014-08-28 15:04:45 -0700

    Guide view: Fix edit on parts and tools
    Any non-step page sould take you to edit guide details. This wasn't the
    case for parts and tools which took you to edit the first step.

--- App/src/com/dozuki/ifixit/ui/guide/view/GuideViewActivity.java
+++ App/src/com/dozuki/ifixit/ui/guide/view/GuideViewActivity.java
@@ -253,8 +253,8 @@ public class GuideViewActivity extends BaseMenuDrawerActivity implements
                App.sendEvent("menu_action", "button_press", "edit_guide", (long)mGuide.getGuideid());
 
                // If the user is on the introduction, take them to edit the introduction fields.
-               // If the user is on the introduction, take them to edit the introduction fields.
-               if (mCurrentPage == 0 || (mCurrentPage - mAdapter.getStepOffset()) >= mGuide.getNumSteps()) {
+               if (mCurrentPage < mAdapter.getStepOffset() ||
+                (mCurrentPage - mAdapter.getStepOffset()) >= mGuide.getNumSteps()) {
                   Intent intent = new Intent(this, GuideIntroActivity.class);
                   intent.putExtra(StepsActivity.GUIDE_KEY, mGuide);
                   intent.putExtra(GuideIntroActivity.STATE_KEY, true);
